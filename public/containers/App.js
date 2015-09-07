c08be3aa4d619b00718d79abea601df1d26e0c43
恒春 <zhiwei.jzw@alibaba-inc.com>
2015-09-07 22:23:32 +0800

    up

--- public/containers/App.js
+++ public/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
@@ -19,10 +19,15 @@ class App extends Component {
   }
 }
 
-function select(state) {
+App.propTypes = {
+  todos: PropTypes.array.isRequired,
+  dispatch: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
   return {
     todos: state.todos
   };
 }
 
-export default connect(select)(App);
+export default connect(mapStateToProps)(App);
